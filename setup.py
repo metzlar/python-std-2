edf0107f0cf73db9ec78848757557560729df6b2
Ivan Metzlar <metzlar@gmail.com>
2014-04-03 14:10:48 -0700

    singleton

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+  name='Python Utilities',
+  version='0.1',
+  py_modules=['std2'],
+  cmdclass={'upload':lambda x:None},
+  install_requires=[
+  ],
+)# pragma: no cover  
